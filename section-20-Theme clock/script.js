45e5a278fa8854c325b2c61249c98e903830d662
nivya <nivyapeter94@gmail.com>
2021-07-21 17:33:39 +0530

    clock style

--- section-20-Theme clock/script.js
+++ section-20-Theme clock/script.js
@@ -43,6 +43,6 @@ const scale = (num, in_min, in_max, out_min, out_max) => {
     return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-setTime()
+ setTime()
 
 setInterval(setTime, 1000)
