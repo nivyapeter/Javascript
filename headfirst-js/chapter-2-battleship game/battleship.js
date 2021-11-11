b83e6e37ee05851511a3f4fb647734b4bf929f90
nivyapeter <nivyakpeter@gmail.com>
2021-11-11 18:18:06 +0530

    battleship game

--- headfirst-js/chapter-2-battleship game/battleship.js
+++ headfirst-js/chapter-2-battleship game/battleship.js
@@ -4,3 +4,20 @@ var location3 = 5;
 var guess;
 var hits = 0;
 var guesses = 0;
+while (isSunk == false) {
+    guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
+    if (guess < 0 || guess > 6) {
+    alert("Please enter a valid cell number!");
+    } else {
+        guesses = guesses + 1
+        if (guesss == location1 || guess == location2 || guess == location3) {
+            hits = hits + 1;
+            if (hits == 3) {
+                issunk = true;
+                alert("you sank my battleship!");
+            }
+        }else {
+                alert("MISS");
+            }
+    }
+}
