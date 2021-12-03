8e1b6930a257020dcbb4cc9c025ca9da136e238b
nivyapeter <nivyakpeter@gmail.com>
2021-12-03 17:45:57 +0530

    dom manipulation

---
+++ headfirst-js/chapter-6-dom manipulation/dom.js
@@ -0,0 +1,11 @@
+// let access = document.getElementById('code9');
+// let code = access.innerHTML;
+// code = code + " " + "midnight";
+// alert(code);
+
+// adding content to dom
+
+let planet = document.getElementById("greenplanet");
+console.log(planet);
+planet.innerHTML="welcome";
+console.log(planet.innerHTML);
